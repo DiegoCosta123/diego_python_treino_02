eb9a0d83d2c106a19fde77bed2dbeee653b173ce
Diego da Costa <diegoccpinto@gmail.com>
2025-11-26 02:37:42 -0300

    Exercise 65 on Highest and Lowest Values

--- Aulas/relembrar/aulaRel01.py
+++ Aulas/relembrar/aulaRel01.py
@@ -279,7 +279,37 @@
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------
-# Exercícios 63 - Sequência de Fibonacci:
+# Exercícios 64 - Maior e Menor valores:
+
+resp = 'S'
+soma = 0
+quant = 0
+media = 0
+maior = 0
+menor = 0
+
+while resp in 'Ss':
+   num = int(input('Digite um número: '))
+
+   soma += num
+   quant += 1
+
+   if quant == 1:
+      maior = menor = num
+   
+   else:
+      if num > maior:
+         maior = num
+      
+      if num < menor:
+         menor = num
+
+   resp = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
 
 
+media = soma / quant
+
+print(f'Você digitou {quant} números e a média foi {media}')
+print(f'Maior número de valor é: {maior}')
+print(f'Menor número de valor é: {menor}')
 
